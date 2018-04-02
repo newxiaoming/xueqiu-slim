2e1646896589ca3d1865c90aa57fcfbf81a68918
清晨的来客 <chn_614si@163.com>
2018-04-03 03:50:23 +0800

    删除php_errors.log 文件

---
+++ app/Home/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 电脑A
+ * Date: 2018/4/3
+ * Time: 3:25
+ */
+
+namespace App\Home\Controllers;
+
+
+class Controller
+{
+
+}
---
+++ app/Home/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Desc: basic controller
+ * User: qingchendelaike
+ * Date: 2018/4/3
+ * Time: 2:49
+ */
+
+namespace App\Home\Controller;
+
+class HomeController
+{
+    public function index()
+    {
+        return "HomeController";
+    }
+}
